52808dde07fa31816b195f88533e9c8433593753
leoAshu <ashutosh.ojha2009@gmail.com>
2023-11-11 14:10:51 +0530

    designs navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <header className="header">
+            <NavLink
+                to="/"
+                className="w-12 h-12 rounded-lg bg-white flex items-center justify-center font-bold shadow-md"
+            >
+                <p className="blue-gradient_text text-lg">AO</p>
+            </NavLink>
+            <nav className="flex text-lg gap-7 font-medium">
+                <NavLink
+                    to="/about"
+                    className={({ isActive }) =>
+                        isActive ? 'text-blue-500' : 'text-black'
+                    }
+                >
+                    About
+                </NavLink>
+                <NavLink
+                    to="/projects"
+                    className={({ isActive }) =>
+                        isActive ? 'text-blue-500' : 'text-black'
+                    }
+                >
+                    Projects
+                </NavLink>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
