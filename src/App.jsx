c029e4e14b6f7549935b2c13bb679e5f0ed1a343
leoAshu <ashutosh.ojha2009@gmail.com>
2023-11-11 16:23:56 +0530

    implements loader and dummy pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
+
+import { About, Contact, Home, Projects } from './pages'
+import Navbar from './components'
 
 const App = () => {
     return (
@@ -7,10 +9,10 @@ const App = () => {
             <Router>
                 <Navbar />
                 <Routes>
-                    <Route path="/" element={'Home'} />
-                    <Route path="/about" element={'About'} />
-                    <Route path="/projects" element={'Projects'} />
-                    <Route path="/contact" element={'Contact'} />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/contact" element={<Contact />} />
                 </Routes>
             </Router>
         </main>
