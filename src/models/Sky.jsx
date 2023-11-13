389e077d8b8742f379b3096ef1e4db8b36296abf
leoAshu <ashutosh.ojha2009@gmail.com>
2023-11-13 22:10:09 +0530

    implements the bird flying animatations and logic

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,8 +1,8 @@
 import { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
 
 import skyScene from '../assets/3d/sky.glb'
-import { useFrame } from '@react-three/fiber'
 
 const Sky = ({ isRotating }) => {
     const skyRef = useRef()
