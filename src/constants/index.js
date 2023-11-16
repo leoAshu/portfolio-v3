565f5c13600e22dfc568300f46a38f8d3e738e22
leoAshu <ashutosh.ojha2009@gmail.com>
2023-11-16 17:50:05 +0530

    designs the projects section in projects page

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,7 @@
 import { amazon, enparadigm, issc, oit } from '../assets/images'
 import {
-    car,
     contact,
     css,
-    estate,
     express,
     git,
     github,
@@ -13,13 +11,11 @@ import {
     mongodb,
     nextjs,
     nodejs,
-    pricewise,
     react,
     sass,
-    snapgram,
     summiz,
+    dalle,
     tailwindcss,
-    threads,
     typescript,
     android,
     flask,
@@ -177,51 +173,21 @@ export const socialLinks = [
 
 export const projects = [
     {
-        iconUrl: pricewise,
-        theme: 'btn-back-red',
-        name: 'Amazon Price Tracker',
-        description:
-            'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
-        link: 'https://github.com/adrianhajdin/pricewise',
-    },
-    {
-        iconUrl: threads,
-        theme: 'btn-back-green',
-        name: 'Full Stack Threads Clone',
-        description:
-            'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-        link: 'https://github.com/adrianhajdin/threads',
-    },
-    {
-        iconUrl: car,
-        theme: 'btn-back-blue',
-        name: 'Car Finding App',
-        description:
-            'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
-        link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
-    },
-    {
-        iconUrl: snapgram,
-        theme: 'btn-back-pink',
-        name: 'Full Stack Instagram Clone',
-        description:
-            'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
-        link: 'https://github.com/adrianhajdin/social_media_app',
-    },
-    {
-        iconUrl: estate,
+        iconUrl: dalle,
         theme: 'btn-back-black',
-        name: 'Real-Estate Application',
+        name: 'DALL-E Clone',
         description:
-            'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
-        link: 'https://github.com/adrianhajdin/projects_realestate',
+            "A MERN app that leverages OpenAI's DALL-E API to generate and share images, fostering a creative and collaborative space.",
+        link: 'https://github.com/leoAshu/dalle_clone',
+        demo: 'https://dalle-clone-khaki.vercel.app/',
     },
     {
         iconUrl: summiz,
-        theme: 'btn-back-yellow',
-        name: 'AI Summarizer Application',
+        theme: 'btn-back-orange',
+        name: 'Sumz',
         description:
-            'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
-        link: 'https://github.com/adrianhajdin/project_ai_summarizer',
+            'Web-based platform that transforms lengthy articles into concise summaries using OpenAI GPT-4.',
+        link: 'https://github.com/leoAshu/ai-summarizer',
+        demo: 'https://leoashu.github.io/ai-summarizer/',
     },
 ]
